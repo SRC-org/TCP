6fdee03fe31499822746178e83c61964d5ce44d3
srpnt3 <27972286+srpnt3@users.noreply.github.com>
2023-05-15 22:43:12 +0200

    register, copy and database commands

--- Interface UI Channel Assignment Table.md
+++ Interface UI Channel Assignment Table.md
@@ -7,51 +7,51 @@ Channels B1-B3 and N1-N6 are reserved for touch.
 
 The Drivetrain can only be connected to instruments and has no GUI
 
-| Ch. | Function |
-| --- | -------- |
-| N 1 | Brakes (0-1) |
-| N 2 | Throttle (0-maxSpeed) |
-| B 1 | Wheel Slip |
+| Ch. | Function                   |
+|-----|----------------------------|
+| N 1 | Brakes (0-1)               |
+| N 2 | Throttle (0-maxSpeed)      |
+| B 1 | Wheel Slip                 |
 | B 5 | My Engine is ON (optional) |
 
 ## Exterior Lighting
 
-| Ch. | Function |
-| --- | -------- |
-| B 4 | Auto Lights (toggle) |
-| B 5 | Head Lights (toggle) |
-| B 6 | High Beams (toggle) |
-| B 7 | Work Lights (toggle) |
-| B 8 | Hazard Lights (toggle) |
+| Ch. | Function                   |
+|-----|----------------------------|
+| B 4 | Auto Lights (toggle)       |
+| B 5 | Head Lights (toggle)       |
+| B 6 | High Beams (toggle)        |
+| B 7 | Work Lights (toggle)       |
+| B 8 | Hazard Lights (toggle)     |
 | B 9 | Decorative Lights (toggle) |
 
 ## Decouple
 
-| Ch. | Function |
-| --- | -------- |
-| B 4 | Decouple (push) |
+| Ch. | Function                         |
+|-----|----------------------------------|
+| B 4 | Decouple (push)                  |
 | B 5 | Decrement decouple number (push) |
 | B 6 | Increment decouple number (push) |
 
 ## Interior Lighting
 
-| Ch. | Function |
-| --- | -------- |
-| B 4 | Enable lights (toggle) |
+| Ch. | Function                      |
+|-----|-------------------------------|
+| B 4 | Enable lights (toggle)        |
 | B 5 | RGB (off) / HSV (on) (toggle) |
-| B 6 | Heaters (toggle) |
-| N 7 | Red / Hue (0-1) |
-| N 8 | Green / Saturation (0-1) |
-| N 9 | Blue / Value (0-1) |
+| B 6 | Heaters (toggle)              |
+| N 7 | Red / Hue (0-1)               |
+| N 8 | Green / Saturation (0-1)      |
+| N 9 | Blue / Value (0-1)            |
 
 ## Doors
 
-| Ch. | Function |
-| --- | -------- |
-| B 4 | Decrement start position (push) |
-| B 5 | Increment start position (push) |
-| B 6 | Decrement end position (push) |
-| B 7 | Increment end position (push) |
-| B 8 | Unlock left doors (toggle) |
-| B 9 | Unlock right doors (toggle) |
-| B 10 | Open selected doors (toggle) |
+| Ch.  | Function                        |
+|------|---------------------------------|
+| B 4  | Decrement start position (push) |
+| B 5  | Increment start position (push) |
+| B 6  | Decrement end position (push)   |
+| B 7  | Increment end position (push)   |
+| B 8  | Unlock left doors (toggle)      |
+| B 9  | Unlock right doors (toggle)     |
+| B 10 | Open selected doors (toggle)    |
--- updateUtility.js
+++ updateUtility.js
@@ -1,95 +1,113 @@
 fs = require('fs')
-data = require('./database.json')
+database = require('./database.json')
 
 const swPath = process.env.APPDATA + '/Stormworks/data/microprocessors'
 let args = process.argv;
 
+// node updateUtility -args
+// -r:	registers a new controller into the database (identifier and group)
+// -c:	copies the files from stormworks data to repository folder
+// -d:	updates database with repository folder contents
+// -i:	generates illustrations and thumbnails from database
+// -s: 	updates steam workshop items
+
 async function start() {
-	if (args.indexOf('-c') > -1) await copy()
-	if (args.indexOf('-d') > -1) await database()
-	if (args.indexOf('-i') > -1) await images()
-	if (args.indexOf('-s') > -1) await steam()
+	if (args.indexOf('-r') > -1) await execRegister(args.indexOf('-r'))
+	if (args.indexOf('-c') > -1) await execCopy()
+	if (args.indexOf('-d') > -1) await execDatabase()
+	if (args.indexOf('-i') > -1) await execImages()
+	if (args.indexOf('-s') > -1) await execSteam()
 }
 
 start().then(() => {
-	fs.writeFileSync('./database.json', JSON.stringify(data))
-	console.log('finished')
+	fs.writeFileSync('./database.json', JSON.stringify(database, null, '\t'))
+	console.log('script finished')
 })
 
-// copy the files
-async function copy() {
-	files = (await fs.promises.readdir(swPath)).filter(file => file.startsWith('SRC-TCP') && file.endsWith('.xml'))
+// Register
+async function execRegister(index) {
+	let wrongFormat = () => console.log('\x1b[33m%s\x1b[0m', 'wrong format, please use: -r controller,controller group,group')
+
+	let arg1 = args[index+1], arg2  = args[index+2]
+	if (!(arg1 && arg2 && !arg1.startsWith('-') && !arg2.startsWith('-'))) return wrongFormat()
+
+	let controllers = arg1.split(','), groups = arg2.split(',')
+	if (controllers.length !== groups.length) return wrongFormat()
+
+	controllers.forEach((c, i) => {
+		if (c === '' || groups[i] === '') return wrongFormat();
+		let g = groups[i]
+		database.controllers[c] = {
+			identifier: c,
+			group: g
+		}
+		if (!fs.existsSync('./' + g + ' Group/')) fs.mkdirSync('./' + g + ' Group/')
+	})
+}
+
+// Copy
+async function execCopy() {
+	let files = (await fs.promises.readdir(swPath)).filter(file => file.startsWith('SRC-TCP') && file.endsWith('.xml'))
 	let promises = []
 
 	files.forEach((file, i) => {
-		//if (!file.startsWith('SRC-TCP') || !file.endsWith('.xml')) return
-
-		let info = generateInfo(file)
-		promises.push(fs.promises.copyFile(swPath + '/' + file, './' + info.group + ' Group/' + file))
+		let fileData = data.fromFile(file)
+		let entry = data.fromDatabase(fileData.identifier)
+		if (!entry || !entry.group) return console.log('\x1b[33m%s\x1b[0m', 'unknown group for controller: \'' + fileData.identifier + '\', controller was not copied, please register it using -r')
+		if (entry.version && entry.version !== fileData.version) promises.push(fs.promises.unlink('./' + entry.group + ' Group/SRC-TCP ' + fileData.identifier + ' v' + entry.version.replaceAll('.', '_') + '.xml')) // replace old version if present
+		entry.version = fileData.version // update version
+		promises.push(fs.promises.copyFile(swPath + '/' + file, './' + entry.group + ' Group/' + file)) // copy file
 	})
-
-	// TODO: replace old versions on version number change
-
 	await Promise.all(promises)
 }
 
-// update database from folders
-async function database() {
-	let dirs = (await fs.promises.readdir('./', { withFileTypes: true })).filter(d => d.isDirectory()).map(d => d.name).filter(d => d.endsWith('Group'))
+// Database
+async function execDatabase() {
+	let dirs = (await fs.promises.readdir('./', {withFileTypes: true})).filter(d => d.isDirectory()).map(d => d.name).filter(d => d.endsWith('Group'))
 	let promises = []
-	let infos = []
+	let controllerData = []
 	dirs.forEach((dir, i) => {
 		let promise = fs.promises.readdir('./' + dir + '/')
-		promise.then(files => files.filter(file => file.startsWith('SRC-TCP') && file.endsWith('.xml')).forEach((file, i) => infos.push(generateInfo(file))))
+		promise.then(files => files.filter(file => file.startsWith('SRC-TCP') && file.endsWith('.xml')).forEach((file, i) => controllerData.push(data.fromFile(file))))
 		promises.push(promise)
 	})
 	await Promise.all(promises)
 
-	infos.forEach(info => {
-		if (data.controllers[info.full] === undefined) {
-			console.log('new controller: \"' + info.full + '\", please insert remaining info manually into the database')
-			data.controllers[info.full] = info
+	controllerData.forEach(info => {
+		if (database.controllers[info.identifier] === undefined) {
+			console.log('\x1b[33m%s\x1b[0m', 'controller with no database entry: \'' + info.identifier + '\', please register it using -r')
+			database.controllers[info.identifier] = info
 			return
 		}
-		updateEntry(data.controllers[info.full], info)
-		//console.log('database entry updated: ' + info.full)
+		for (let attr in info) database.controllers[info.identifier][attr] = info[attr]
 	})
 }
 
-async function images() {
+// Images
+async function execImages() {
 	console.log('image generation not supported yet')
 }
 
-async function steam() {
+// Steam
+async function execSteam() {
 	console.log('steam upload not supported yet')
 }
 
-// node updateUtility -c -d -i -s
-// -c or --copy:		copies the files from stormworks data to git folder
-// -d or --database:	updates database with git folder contents
-// -i or --images:		generates illustrations and thumbnails from database
-// -s or --steam: 		updates steam workshop items
-
-function generateInfo(file) {
-	let info = file.matchAll(/(?<=\[)(.*?)(?=\] )(?:\] )(.*?)(?=\.| \(| v[\d\_]*\.xml)(?:(?: \()([a-zA-Z\s]+)(?:\)))?(?: v)?([\d\_]*)?/g).next().value // i will forget how this works tomorrow
-	return {
-		full: '[' + info[1] + '] ' + info[2] + (info[3] ? ' (' + info[3] + ')' : ''),
-		group: getGroup(info),
-		type: info[1],
-		name: info[2],
-		modifier: info[3],
-		version: (info[4] ? info[4].replaceAll('_', '.') : undefined)
+data = {
+	matchInfo: (file) => {
+		return file.matchAll(/(?<=\[)(.*?)(?=\] )(?:\] )(.*?)(?=\.| \(| v[\d\_]*\.xml)(?:(?: \()([a-zA-Z\s]+)(?:\)))?(?: v)?([\d\_]*)?/g).next().value // i will forget how this works tomorrow
+	},
+	fromFile: (file) => {
+		let info = data.matchInfo(file)
+		return {
+			identifier: '[' + info[1] + '] ' + info[2] + (info[3] ? ' (' + info[3] + ')' : ''),
+			type: info[1],
+			name: info[2],
+			modifier: info[3],
+			version: (info[4] ? info[4].replaceAll('_', '.') : undefined)
+		}
+	},
+	fromDatabase: (identifier) => {
+		return database.controllers[identifier]
 	}
-}
-
-function getGroup(info) {
-	if (info[1] === 'System') return 'System'
-	if (info[2].startsWith('Passenger')) return 'Passenger'
-	if (info[2].startsWith('Decouple')) return 'Decouple'
-	if (info[2].startsWith('Drivetrain')) return 'Drivetrain'
-	if (info[2].startsWith('Exterior Lighting') || info[2].startsWith('Lightbox')) return 'Lighting'
-}
-
-function updateEntry(entry, info) {
-	entry.version = info.version
 }
